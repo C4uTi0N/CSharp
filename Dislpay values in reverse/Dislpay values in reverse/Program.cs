987fe2a80c5d881d4cf647055d51877ab7b6c79e
C4uTi0N <petec4uti0n@gmail.com>
2021-03-04 22:17:15 +0100

    Finished "Display Values in Reverse"

---
+++ Dislpay values in reverse/Dislpay values in reverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Dislpay_values_in_reverse
+{
+    class Program
+    {
+        static Stack<string> values = new Stack<string>();
+
+        static void Main()
+        {
+            Console.WriteLine("Type any 5 values");
+
+            values.Push(Console.ReadLine());
+            values.Push(Console.ReadLine());
+            values.Push(Console.ReadLine());
+            values.Push(Console.ReadLine());
+            values.Push(Console.ReadLine());
+
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
